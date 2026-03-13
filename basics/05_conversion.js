2dbefba22e2daa0f3676d8689ccb98c44430493f
robinrawat30 <robinrawat4201@gmail.com>
2026-03-13 16:54:09 +0530

    comparison operators == and ===

--- basics/05_conversion.js
+++ basics/05_conversion.js
@@ -23,3 +23,18 @@ console.log(valueInNumber);
 
 
 // same as above also for boolean , string and all by keyword of conversion like Boolean(value) , String(value) etc.
+
+
+
+// operators and checks
+
+
+// ==   => only value check not data type check (loose equality operator)
+
+
+console.log("1"==1); // true
+
+
+// ===   => value check and data type check (strict equality operator)
+
+console.log("1"===1)  //false
