fc70f2308454704cac281f53ca38ef92d75d3341
robinrawat30 <robinrawat4201@gmail.com>
2026-03-15 15:06:10 +0530

    working of numbers in js

---
+++ basics/07_nums_strings.js
@@ -0,0 +1,16 @@
+const score = 400;
+
+
+const balance = new Number(10000);
+
+console.log(balance);
+
+console.log(score);
+
+console.log(balance.toString());
+
+console.log(balance.toFixed(3));
+
+const number = 123.8934;
+
+console.log(number.toPrecision(3));
